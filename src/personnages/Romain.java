66e3a0c0842b4f83b40390da7fa117dfd20c6488
Vika Yarmolich <viktoriia.yarmolich@univ-tlse3.fr>
2023-09-30 21:38:08 +0200

    creation of the enum Equipement

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -71,6 +71,7 @@ public class Romain {
 	
 	public static void main(String[] args) {
 		Romain minus = new Romain("Minus", 6);
-
+		System.out.println(Equipement.CASQUE);
+		System.out.println(Equipement.BOUCLER);
 	}
 }
