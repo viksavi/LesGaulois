abc5223da7fe8172add0fd1411a6d13ab73ab962
Vika Yarmolich <viktoriia.yarmolich@univ-tlse3.fr>
2023-10-01 15:59:46 +0200

    les énumérés

--- src/personnages/Equipement.java
+++ src/personnages/Equipement.java
@@ -1,5 +1,17 @@
 package personnages;
 
 public enum Equipement {
-	CASQUE, BOUCLER;
+	CASQUE("casque"), BOUCLIER("bouclier");
+	
+	private String nom;
+	
+	private Equipement(String nom)
+	{
+		this.nom = nom;
+	}
+	
+	public String toString()
+	{
+		return nom;
+	}
 }
--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,10 +3,13 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement[] equipement;
+	private int nbEquipement = 0;
 
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
+		equipement = new Equipement[2];
 		
 		assert isInvariantSatisfied();
 	}
@@ -54,6 +57,33 @@ public class Romain {
 			return false;
 		}
 	}
+	
+	private void ajouterEquipement(Equipement equipement, int num)
+	{
+		this.equipement[num] = equipement;
+		nbEquipement++;
+		System.out.println("Le soldat " + nom + " s’équipe avec un " + equipement.toString() + "!");
+	}
+	
+	private void sEquiper(Equipement equipement)
+	{
+		switch (nbEquipement) {
+		case 2: {
+			System.out.println("Le soldat " + nom + " est déjà bien protégé!");
+			break;
+		}
+		case 1: {
+			if (equipement == this.equipement[0]) {
+				System.out.println("Le soldat " + nom + "  possède déjà un " + equipement.toString() + "!");
+			} else {
+				ajouterEquipement(equipement, 1);
+			}
+			break;
+		}
+		default:
+			ajouterEquipement(equipement, 0);
+		}
+	}
 
 	private boolean isInvariantSatisfied() {
 		return forceEstPositive();
@@ -72,6 +102,10 @@ public class Romain {
 	public static void main(String[] args) {
 		Romain minus = new Romain("Minus", 6);
 		System.out.println(Equipement.CASQUE);
-		System.out.println(Equipement.BOUCLER);
+		System.out.println(Equipement.BOUCLIER);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.BOUCLIER);
+		minus.sEquiper(Equipement.BOUCLIER);
 	}
 }
