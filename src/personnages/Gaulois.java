cfdfbbe1562e2a3a36ef68025dd4f68c6050d709
Vika Yarmolich <viktoriia.yarmolich@univ-tlse3.fr>
2023-10-02 16:29:54 +0200

    correction de class Gaulois

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -3,7 +3,9 @@ package personnages;
 public class Gaulois {
 	private String nom;
 	private int force;
+	private int nbtrophees;
 	private int effetPotion = 1;
+	private Equipement[] trophees = new Equipement[100];
 
 	public Gaulois(String nom, int force) {
 		this.nom = nom;
@@ -18,17 +20,30 @@ public class Gaulois {
 		System.out.println(prendreParole() + "« " + texte + "»");
 	}
 
-	public String prendreParole() {
+	/*
+	 * public String prendreParole() { return "Le gaulois " + nom + " : "; }
+	 */
+	private String prendreParole() {
 		return "Le gaulois " + nom + " : ";
-	}
+		}
 
+		/*
+		 * public void frapper(Romain romain) { System.out.println(nom +
+		 * " envoie un grand coup dans la mâchoire de " + romain.getNom());
+		 * romain.recevoirCoup((force / 3)*effetPotion); }
+		 */
 	public void frapper(Romain romain) {
 		System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
-		romain.recevoirCoup((force / 3)*effetPotion);
-	}
+		trophees = romain.recevoirCoup((force / 3) * effetPotion);
+		for (int i = 0; trophees != null && i < trophees.length; i++, nbtrophees++) {
+			this.trophees[nbtrophees] = trophees[i];
+			}
+		}
 
-	 public String toString() { return "Gaulois [nom=" + nom + ", force=" + force
-	 + ", effetPotion=" + effetPotion + "]"; }
+		/*
+		 * public String toString() { return "Gaulois [nom=" + nom + ", force=" + force
+		 * + ", effetPotion=" + effetPotion + "]"; }
+		 */
 	 
 
 	public static void main(String[] args) {
